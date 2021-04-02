a97f9b358e5870327d6e10552901baa04019c947
Luke Bredeson <luke.bredeson@gmail.com>
2021-04-01 22:43:07 -0500

    Removed unnecessary cookie

--- index.js
+++ index.js
@@ -7,53 +7,36 @@ const client = require('twilio')(config.twilio.accountSid, config.twilio.authTok
 
 const https = require('https');
 
-let cookieRequest = https.request({
+let request = https.request({
   host: 'www.cvs.com',
-  path: `/immunizations/covid-19-vaccine`,
+  path: `/immunizations/covid-19-vaccine.vaccine-status.${config.cvs.state.abbreviation}.json?vaccineinfo=`,
   headers: {
+	  "Referer": "https://www.cvs.com/immunizations/covid-19-vaccine",
 	  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0"
   }
-}, cookieResponse => {
-	console.log(`Grabbing a cookie...${new Date()}`);
-	let cookie = cookieResponse.headers['set-cookie'].find(cookie => cookie.includes("_abck"));
-	
-	let request = https.request({
-	  host: 'www.cvs.com',
-	  path: `/immunizations/covid-19-vaccine.vaccine-status.${config.cvs.state.abbreviation}.json?vaccineinfo=`,
-	  headers: {
-		  "Referer": "https://www.cvs.com/immunizations/covid-19-vaccine",
-		  "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0",
-		  "Cookie": cookie
-	  }
-	}, response => {
-	  response.on('data', d => {
-		console.log(`Looking for CVS vaccines...${new Date()}`);
-		let json = JSON.parse(d);
-		let pharmacies = json.responsePayloadData.data.MN;
-		for (i in pharmacies) {
-			let pharmacy = pharmacies[i];
-			if (pharmacy.status !== "Fully Booked") {
-				console.log("Found a pharmacy that isn't 'Fully Booked'");
-				console.log(pharmacy);
-			
-				for (j in config.twilio.smsReceivers) {
-					sendSms(config.twilio.smsReceivers[j], `Found an open CVS pharmacy in ${pharmacy.city}, please visit https://www.cvs.com/immunizations/covid-19-vaccine and click on ${config.cvs.state.name} to schedule an appointment.`);
-				}
+}, response => {
+  response.on('data', d => {
+	console.log(`Looking for CVS vaccines...${new Date()}`);
+	let json = JSON.parse(d);
+	let pharmacies = json.responsePayloadData.data.MN;
+	for (i in pharmacies) {
+		let pharmacy = pharmacies[i];
+		if (pharmacy.status !== "Fully Booked") {
+			console.log("Found a pharmacy that isn't 'Fully Booked'");
+			console.log(pharmacy);
+		
+			for (j in config.twilio.smsReceivers) {
+				sendSms(config.twilio.smsReceivers[j], `Found an open CVS pharmacy in ${pharmacy.city}, please visit https://www.cvs.com/immunizations/covid-19-vaccine and click on ${config.cvs.state.name} to schedule an appointment.`);
 			}
 		}
-	  });	
-	});
-
-	request.on('error', error => {
-	  console.error(error)
-	})
-	request.end();
+	}
+  });	
 });
 
-cookieRequest.on('error', error => {
+request.on('error', error => {
   console.error(error)
 })
-cookieRequest.end();
+request.end();
 
 let sendSms = (to, message) => {
 	client.messages
